6f38b49de03d1cc3dda227cb0248f35cd85e4b61
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2018-12-04 10:13:11 +0000

    modified sample_spec.js

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,5 +1,22 @@
+// describe('My First Test', function() {
+//   it('Does not do much!', function() {
+//     expect(true).to.equal(true)
+//   })
+// })
+
+
+// describe('My First Test', function() {
+//   it('Visits the Kitchen Sink', function() {
+//     cy.visit('https://example.cypress.io')
+//   })
+// })
+
 describe('My First Test', function() {
-  it('Does not do much!', function() {
-    expect(true).to.equal(true)
+  it('finds the content "type"', function() {
+    cy.visit('https://example.cypress.io')
+
+    cy.contains('type')
+    cy.contains('children').click()
+    cy.url().should('include', '/commands/traversal')
   })
 })
