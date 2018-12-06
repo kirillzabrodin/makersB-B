470d7a49703bd1467a1670047ab3736e1994e441
Tomas Doherty <tomasdoh1@gmail.com>
2018-12-06 12:14:07 +0000

    Minor changes to Sign up and Log in

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,12 @@ import { Button, Alert } from "reactstrap";
 export default () => {
   return (
     <div>
+    <Alert color="primary">
+      Hi Lorna!
+    </Alert>
     <h1>Welcome to MakersBnB</h1>
+    <br />
+    <Button color="primary">Hello</Button>
     </div>
   );
 };
